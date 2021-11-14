ce36c56bd42719557bc10c40d069b624162d35ec
adisen99 <adityasenguptasai99@gmail.com>
2021-11-15 00:54:08 +0530

    feat: added new part server_progress, remove status part

--- lua/hardline/parts/server_progress.lua
+++ lua/hardline/parts/server_progress.lua
@@ -1,48 +1,8 @@
 local has_lsp_status, lsp_status = pcall(require, 'lsp-status')
 
-local nvim_buf_get_var = function(bufnr, var_name)
-  local ok, result = pcall(function()
-    return vim.api.nvim_buf_get_var(bufnr, var_name)
-  end)
-
-  if ok then
-    return result
-  end
-end
-
-local helper = function(var_name)
-  return function(_, buffer)
-    return nvim_buf_get_var(buffer.bufnr, var_name)
-  end
-end
-
-local get_current_function = helper('lsp_current_function')
-
 local M = {}
 
-M.segment = function(_, buffer)
-  if not buffer.lsp or not has_lsp_status then
-    return ''
-  end
-
-  local ok, result = pcall(lsp_status.status)
-  return ok and result or ''
-end
-
-M.current_function = function(_, buffer)
-  if not buffer.lsp or not has_lsp_status then
-    return ''
-  end
-
-  local ok, current_func = pcall(get_current_function, _, buffer)
-  if ok and current_func and #current_func > 0 then
-    return string.format('[ %s ]', current_func)
-  end
-
-  return ''
-end
-
-M.server_progress = function(_, bufnr)
+M.get_item = function(_, bufnr)
   if vim.inspect(vim.lsp.buf_get_clients()) == {} or not has_lsp_status then
     return ''
   end
