5b2b27bfdaed35d9aa5b8278ba887a02fe99e778
adisen99 <adityasenguptasai99@gmail.com>
2021-12-04 11:55:20 +0530

    feat: separating file_icon and filetype.

---
+++ lua/hardline/parts/fileicon.lua
@@ -0,0 +1,28 @@
+local function get_file_info()
+  return vim.fn.expand('%:t'), vim.fn.expand('%:e')
+end
+
+local function get_file_icon()
+  local icon = ''
+  if vim.fn.exists("*WebDevIconsGetFileTypeSymbol") == 1 then
+    icon = vim.fn.WebDevIconsGetFileTypeSymbol()
+    return icon .. ' '
+  end
+  local ok,devicons = pcall(require,'nvim-web-devicons')
+  if not ok then print('No icon plugin found. Please install \'kyazdani42/nvim-web-devicons\'') return '' end
+  local f_name,f_extension = get_file_info()
+  icon = devicons.get_icon(f_name,f_extension)
+  if icon == nil then
+      icon = ''
+  end
+  return icon .. ' '
+end
+
+local function get_item()
+ local icon = get_file_icon()
+ return icon
+end
+
+return {
+  get_item = get_item,
+}
