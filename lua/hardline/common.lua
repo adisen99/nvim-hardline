6245651b15227b86e29cac0486ccb44b956714a3
adisen99 <adityasenguptasai99@gmail.com>
2021-12-10 00:29:00 +0530

    feat: testing new modes

--- lua/hardline/common.lua
+++ lua/hardline/common.lua
@@ -3,7 +3,8 @@ local g = vim.g
 local fmt = string.format
 local M = {}
 
-M.modes = {
+-- old modes code
+--[[ M.modes = {
   ['?'] = {text = '???', state = 'inactive'},
   ['n'] = {text = 'NORMAL', state = 'normal'},
   ['i'] = {text = 'INSERT', state = 'insert'},
@@ -16,6 +17,30 @@ M.modes = {
   ['S'] = {text = 'S-LINE', state = 'visual'},
   [''] = {text = 'S-BLOCK', state = 'visual'},
   ['t'] = {text = 'TERMINAL', state = 'command'},
+} ]]
+
+-- new modes
+M.modes = {
+   n      = {'Normal', 'N', 'NormalMode'},
+   no     = {'N·OpPd', '?', 'OpPending' },
+   v      = {'Visual', 'V', 'VisualMode'},
+   V      = {'V·Line', 'Vl', 'VisualLineMode'},
+   [''] = {'V·Blck', 'Vb' },
+   s      = {'Select', 'S' },
+   S      = {'S·Line', 'Sl' },
+   [''] = {'S·Block', 'Sb' },
+   i      = {'Insert', 'I', 'InsertMode'},
+   ic     = {'ICompl', 'Ic', 'ComplMode'},
+   R      = {'Rplace', 'R', 'ReplaceMode'},
+   Rv     = {'VRplce', 'Rv' },
+   c      = {'Cmmand', 'C', 'CommandMode'},
+   cv     = {'Vim Ex', 'E' },
+   ce     = {'Ex (r)', 'E' },
+   r      = {'Prompt', 'P' },
+   rm     = {'More  ', 'M' },
+   ['r?'] = {'Cnfirm', 'Cn'},
+   ['!']  = {'Shell ', 'S'},
+   t      = {'Term  ', 'T', 'TerminalMode'},
 }
 
 function M.echo(hlgroup, msg)
