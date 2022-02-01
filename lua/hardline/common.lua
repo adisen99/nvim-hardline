154348e7c0deeae9afab4323ac120559da3aba3d
adisen99 <adityasenguptasai99@gmail.com>
2022-02-01 23:29:24 +0530

    fix: refactoring using original repo

--- lua/hardline/common.lua
+++ lua/hardline/common.lua
@@ -1,25 +1,6 @@
-local cmd, fn = vim.cmd, vim.fn
-local g = vim.g
 local fmt = string.format
 local M = {}
 
--- old modes code
---[[ M.modes = {
-  ['?'] = {text = '???', state = 'inactive'},
-  ['n'] = {text = 'NORMAL', state = 'normal'},
-  ['i'] = {text = 'INSERT', state = 'insert'},
-  ['R'] = {text = 'REPLACE', state = 'replace'},
-  ['v'] = {text = 'VISUAL', state = 'visual'},
-  ['V'] = {text = 'V-LINE', state = 'visual'},
-  [''] = {text = 'V-BLOCK', state = 'visual'},
-  ['c'] = {text = 'COMMAND', state = 'command'},
-  ['s'] = {text = 'SELECT', state = 'visual'},
-  ['S'] = {text = 'S-LINE', state = 'visual'},
-  [''] = {text = 'S-BLOCK', state = 'visual'},
-  ['t'] = {text = 'TERMINAL', state = 'command'},
-} ]]
-
--- new modes
 M.modes = {
    ['?']      = {text = '???', state = 'inactive'},
    n      = {text = 'Normal', state = 'normal'},
@@ -45,20 +26,16 @@ M.modes = {
 }
 
 function M.echo(hlgroup, msg)
-  cmd(fmt('echohl %s', hlgroup))
-  cmd(fmt('echo "[hardline] %s"', msg))
-  cmd('echohl None')
-end
-
-function M.is_active()
-  return g.statusline_winid == fn.win_getid()
+  vim.api.nvim_echo({{fmt('[hardline] %s', msg), hlgroup}}, false, {})
 end
 
 function M.set_cache_autocmds(augroup)
-  cmd(fmt('augroup %s', augroup))
-  cmd('autocmd!')
-  cmd(fmt('autocmd CursorHold,BufWritePost * unlet! b:%s', augroup))
-  cmd('augroup END')
+  vim.cmd(fmt([[
+  augroup %s
+    autocmd!
+    autocmd CursorHold,BufWritePost * unlet! b:%s
+  augroup END
+  ]], augroup, augroup))
 end
 
 return M
